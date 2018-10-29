aea2f9cb8f7e5c8a3c0834aa2da1ea3223852dc5
bayyuaji <bayyuaji@gmail.com>
2018-10-30 00:08:04 +0700

    test build 4

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-eval "$(ssh-agent -s)"
-ssh-add -k ~/.ssh/id_rsa
+eval "$(ssh-agent -s)" &&
+ssh-add -k ~/.ssh/id_rsa &&
 
-cd $ROOT_DIR
-git pull
-npm install 
+cd $ROOT_DIR &&
+git pull && 
+npm install &&
 npm run build

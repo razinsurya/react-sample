3c9a526fe4433d448dcc7d1f000522653bf5f28e
bayyuaji <bayyuaji@gmail.com>
2018-10-30 14:54:28 +0700

    belajar CI

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Belajar React
+            Belajar ReactJS
           </a>
         </header>
       </div>

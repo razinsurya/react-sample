1598a1d481d93e2201467f4eeb21b60fb6458897
Bayu Aji <bayyuaji@gmail.com>
2019-04-04 12:11:05 +0700

    demo reeak

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Demo Belajar ReactJS
+            Demo Belajar ReakJS
           </a>
         </header>
       </div>
